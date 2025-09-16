ac5e217ef99d796e93087323af33ad91c217f512
cesar <cesarjosecote@gmail.com>
2025-09-16 09:22:25 -0500

    init

---
+++ tramites/src/test/java/com/example/tramites/TramitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tramites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
